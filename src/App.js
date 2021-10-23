15e2798ca30ab7ee7d2797a570d8fad040153743
Songwenbo <git_wenbo@163.com>
2021-10-23 11:04:09 +0800

    测试轮播图数据

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Layout from './pages/home/Layout'
 import "assets/style/reset.css"
-
+import {Provider} from "react-redux"
+import store from "./store/"
 
 function App() {
   return (
-   
+    <Provider store={store}>
     <div className="App">
        <Layout/>
     </div>
-
+    </Provider>
   );
 }
 
--- src/pages/home/cookbook/swiper.jsx
+++ src/pages/home/cookbook/swiper.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import {Carousel} from "antd-mobile"
-import swiper from "assets/swiper-1.png"
+
 import {SwiperWrap} from "./styledCookbook"
 
-export default class Swiper extends Component {
+import { connect } from 'react-redux'
+
+
+const mapState=state=>({
+  list:state.list
+})
+ class Swiper extends Component {
   render() {
     return (
         <SwiperWrap>
@@ -13,9 +19,12 @@ export default class Swiper extends Component {
       beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
       afterChange={index => console.log(`slide to`,index)}
       >
-    <img src={swiper}/>
+       {
+           this.props.list.slice(0.5).map((value)=> <img key={value.id} src={value.img} />)
+       }
       </Carousel>
       </SwiperWrap>
     )
   }
 }
+export default connect(mapState)(Swiper)
